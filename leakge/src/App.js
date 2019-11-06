ddafe4468293523acd3610222c5b23c678aaff94
Chenyu-Shi <shounshi2017@gmail.com>
2019-11-05 20:21:32 -0800

    loop worked

--- leakge/src/App.js
+++ leakge/src/App.js
@@ -6,6 +6,10 @@ class Game extends React.Component{
   render(){
     return (
       <div>
+      <audio loop autoplay = "autoplay" >
+        <source src="http://soundbible.com/mp3/Orca-SoundBible.com-841491912.mp3" type = 'audio/mp3' >
+        </source>
+      </audio>
       <Country />
       <AdviceColumn />
       <StatusBoard />
@@ -20,7 +24,7 @@ class Country extends React.Component{
     return (
       <div>
       <p> This is a country </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-mPmUyeQfCJk%2FUB3wkAaJpJI%2FAAAAAAAAASU%2FRJk-oqNkKDE%2Fs1600%2Fmap-of-world.jpg&f=1&nofb=1" className = "image" alt="photo" width ="300" height = "300"/>
+      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-mPmUyeQfCJk%2FUB3wkAaJpJI%2FAAAAAAAAASU%2FRJk-oqNkKDE%2Fs1600%2Fmap-of-world.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
       </div>
       );
   }
@@ -31,7 +35,7 @@ class StatusBoard extends React.Component{
     return (
       <div>
       <p> This is the statusboard  </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.panic.com%2Fblog%2Fwp-content%2Fuploads%2F2013%2F04%2Fsb-panic-3.jpg&f=1&nofb=1" className = "image" alt="photo" width ="300" height = "300"/>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.panic.com%2Fblog%2Fwp-content%2Fuploads%2F2013%2F04%2Fsb-panic-3.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
       </div>
       );
   }
@@ -42,7 +46,7 @@ class AdviceColumn extends React.Component{
     return (
       <div>
       <p> This is the AdviceColumn </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.images.express.co.uk%2Fimg%2Fdynamic%2F71%2F590x%2Fsecondary%2FBMW-PGA-Championship-latest-leaderboard-Wentworth-948805.jpg&f=1&nofb=1" className = "image" alt="photo" width ="300" height = "300"/>
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.images.express.co.uk%2Fimg%2Fdynamic%2F71%2F590x%2Fsecondary%2FBMW-PGA-Championship-latest-leaderboard-Wentworth-948805.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
       </div>
       );
   }
