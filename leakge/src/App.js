5730a173af838e822b3acc5eee4df7206f879bf9
Chenyu-Shi <shounshi2017@gmail.com>
2019-11-25 18:27:57 -0800

    send it

--- leakge/src/App.js
+++ leakge/src/App.js
@@ -10,58 +10,40 @@ class Game extends React.Component{
         <source src="http://soundbible.com/mp3/Orca-SoundBible.com-841491912.mp3" type = 'audio/mp3' >
         </source>
       </audio>
-      <Country />
-      <AdviceColumn />
-      <StatusBoard />
+      <TextBar />
+      <Button />
       </div>
       );
   }
-}
+};
 
-class Country extends React.Component{
+class Button extends React.Component{
+  
   constructor(props){
     super(props);
     this.state = {
-      GDP: 100, 
-      Emissions: 100, 
-      HealthBar: 100,
-      CarbonPolicy: "None",
-      defaultPolicy: "None",
-      MeetingInternationalPolicy = true 
-    }
+    InitialWord : "Start the game."
+  }; 
+  }
+  render(){
+    return <button id="start"> {this.state.InitialWord} </button>
+  }
+
+}
+
+class TextBar extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+    InitialWord : "You just started this game."
+  }; 
   }
 
   render(){
-    return (
-      <div>
-      <p> This is a country </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2F3.bp.blogspot.com%2F-mPmUyeQfCJk%2FUB3wkAaJpJI%2FAAAAAAAAASU%2FRJk-oqNkKDE%2Fs1600%2Fmap-of-world.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
-      </div>
-      );
+    return <p> {this.state.InitialWord} </p>
   }
 }
 
-class StatusBoard extends React.Component{
-  render(){
-    return (
-      <div>
-      <p> This is the statusboard  </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.panic.com%2Fblog%2Fwp-content%2Fuploads%2F2013%2F04%2Fsb-panic-3.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
-      </div>
-      );
-  }
-}
-
-class AdviceColumn extends React.Component{
-  render(){
-    return (
-      <div>
-      <p> This is the AdviceColumn </p>
-      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.images.express.co.uk%2Fimg%2Fdynamic%2F71%2F590x%2Fsecondary%2FBMW-PGA-Championship-latest-leaderboard-Wentworth-948805.jpg&f=1&nofb=1" className = "image" alt="photo" width ="400" height = "300"/>
-      </div>
-      );
-  }
-}
 
 function App() {
   // Step 1 create country instance
@@ -70,10 +52,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload Hello world.
-        </p>
+
         <Game
           />
         <a
