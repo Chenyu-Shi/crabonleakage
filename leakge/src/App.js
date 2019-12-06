2138e101d9f3489d7e1445a0a0c1e4abcacb5031
Chenyu-Shi <shounshi2017@gmail.com>
2019-12-05 19:51:22 -0800

    adding herman code

--- leakge/src/App.js
+++ leakge/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+​
 class Game extends React.Component{
   render(){
     return (
@@ -16,8 +16,8 @@ class Game extends React.Component{
       );
   }
 };
-
-
+​
+​
 class Button extends React.Component{
   
   constructor(props){
@@ -31,12 +31,12 @@ class Button extends React.Component{
   }; 
     this.handleClick = this.handleClick.bind(this);
   }
-
+​
   handleClick(){
-
+​
     if(this.state.Stage == "Init"){
       this.setState({InitialWord: "The game has begun", Stage:"Intro"});
-
+​
       changeText("This is your country's state right now: your gdp is " + this.state.Player.gdp + " trillion dollars. Your carbon emission is " + this.state.Player.carbon + ", your carbon policy is " + this.state.Player.policy );
     
     }
@@ -48,14 +48,14 @@ class Button extends React.Component{
   render(){
     return <button id="start" style={{width:"200px",height:"50px",boxShadow: "0 8px 16px 0 rgba(0,0,0,0.2), 0 6px 20px 0 rgba(0,0,0,0.19)",fontSize: "20px"}} onClick = {this.handleClick}> {this.state.InitialWord} </button>
   }
-
+​
 }
-
-
+​
+​
 function changeText(word){
     this.setState({InitialWord:word});
   }
-
+​
 class TextBar extends React.Component {
   constructor(props){
     super(props);
@@ -64,17 +64,43 @@ class TextBar extends React.Component {
   };
     changeText = changeText.bind(this);
   }
-
-
+​
+​
   render(){
     return <div style={{width:"800px"}}><p> {this.state.InitialWord} </p></div>
   }
 }
-
-
+​
+class Policy extends React.Component {
+  
+}
+​
+function roundUpdate() {
+​
+}
+​
+function acceptAgreement() {
+​
+}
+​
+class Country extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      GDP : 0,
+      Emissions : 0,
+      Score : 0,
+      CurrentPolicy : "",
+      DefaultPolicy : "",
+      MeetingInternationalAgreement : true
+    }
+  }
+}
+​
+​
 function App() {
   // Step 1 create country instance
-
+​
   // step 2 call country's 
   return (
     <div className="App">
@@ -85,5 +111,5 @@ function App() {
     </div>
   );
 }
-
+​
 export default App;
